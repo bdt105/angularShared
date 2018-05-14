21bdeb5cce4201b7e2d791170c25bb8d2aa755b8
bdt105@gmail.com <bdt150@gmail.com>
2018-05-14 08:49:19 +0200

    .

--- services/auth.guard.ts
+++ services/auth.guard.ts
@@ -9,12 +9,12 @@ export class AuthGuard implements CanActivate{
 
     private toolbox: Toolbox = new Toolbox();
     
-    constructor(private router: Router, public connexionService: ConnexionTokenService){
+    constructor(private router: Router, public connexionTokenService: ConnexionTokenService){
 
     }
 
     canActivate (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        let conn = this.connexionService.isConnected();
+        let conn = this.connexionTokenService.isConnected();
         if (!conn){
             this.toolbox.writeToStorage("redirectUrl", state.url, true);
             this.router.navigate(['/login']);
